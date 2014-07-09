520c729b3b2fd289358f1ac74a3c86648f47c684
Kirubaharan <haran.kiruba@gmail.com>
2014-07-09 10:52:35 +0530

    made savefig to work

--- stage_volume_607.py
+++ stage_volume_607.py
@@ -7,6 +7,8 @@ from spread import spread
 import itertools
 from matplotlib import rc
 
+#function to create iterator series
+
 
 def pairwise(iterable):
     """s -> (s0,s1), (s1,s2), (s2,s3), ..."""
@@ -14,6 +16,7 @@ def pairwise(iterable):
     next(b, None)
     return itertools.izip(a, b)
 
+#function to create stage volume output
 
 def calcvolume(profile, order, dy):
     """Profile = df.Y1,df.Y2,.. and order = 1,2,3"""
@@ -31,36 +34,35 @@ def calcvolume(profile, order, dy):
         calc_vol = water_area * dy
         results.append(calc_vol)
     output[('Volume_%s' % order)] = results
-
+#inout parameters
 check_dam_no = 607
 check_dam_height = 2
 no_of_stage_interval = check_dam_height/.05
 
+#create a list of stage values with 5 cm interval
 dz = list((spread(0.00, check_dam_height, int(no_of_stage_interval), mode=3)))
 
+#surveyed data
 input_file = '/media/kiruba/New Volume/r/r_dir/stream_profile/new_code/created_profile_607.csv'
 df = pd.read_csv(input_file, header=0)
-row = 17
+row = 17   # row of Y values
 
-index = [range(1, 42, 1)]
+index = [range(1, 42, 1)]  # no of stage intervals
 columns = ['stage_m']
 data = np.array(dz)
-output = pd.DataFrame(data, index=index, columns=columns)
+output = pd.DataFrame(data, index=index, columns=columns)  # dataframe with stage values
 
 for l1, l2 in pairwise(df.ix[row]):
     if l2 > 0:
         calcvolume(profile=df["Y_%s" % int(l1)], order=l1, dy=int(l2-l1))
 
-output_series = output.filter(regex="Volume_")
-output["total_vol"] = output_series.sum(axis=1)
+output_series = output.filter(regex="Volume_")  # filter the columns that have Volume_
+output["total_vol"] = output_series.sum(axis=1)  # get total volume
 print output.head(5)
-output.to_csv('/media/kiruba/New Volume/r/r_dir/stream_profile/new_code/stage_vol_607.csv', sep=",")
+output.to_csv('/media/kiruba/New Volume/r/r_dir/stream_profile/new_code/stage_vol_607.csv', sep=",")  #output file
+### Plotting
 plt.plot(output['stage_m'], output['total_vol'], label="Stage - Volume")
 plt.legend(loc='upper left')
-# plt.xlabel('Stage (m)')
-# plt.ylabel('Total Volume (cu.m')
-# plt.title('Stage - volume relationship curve for Check Dam - 634')
-##add axis labels
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 rc('text', usetex=True)
 plt.rc('text', usetex=True)
@@ -68,5 +70,5 @@ plt.rc('font', family='serif')
 plt.xlabel(r'\textbf{Stage} (m)')
 plt.ylabel(r'\textbf{Volume} ($m^3$)')
 plt.title(r"Stage - Volume Relationship for Check Dam - %s" % check_dam_no, fontsize=16)
+plt.savefig('/media/kiruba/New Volume/r/r_dir/stream_profile/new_code/function/stage_vol_607')
 plt.show()
-plt.savefig('/media/kiruba/New Volume/r/r_dir/stream_profile/new_code/function/stage_vol_634.png')
