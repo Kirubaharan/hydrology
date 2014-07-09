4eda1ae06da6a73c59bac33818a06ca03adfe845
Kirubaharan <haran.kiruba@gmail.com>
2014-07-09 14:33:46 +0530

    sep file for functions calcvolume,pairwise

--- stage_volume_607.py
+++ stage_volume_607.py
@@ -19,7 +19,14 @@ def pairwise(iterable):
 #function to create stage volume output
 
 def calcvolume(profile, order, dy):
-    """Profile = df.Y1,df.Y2,.. and order = 1,2,3"""
+    """
+    Profile = df.Y1,df.Y2,.. and order = 1,2,3
+    :param profile: series of Y values
+    :param order: distance from origin
+    :param dy: thickness of profile in m
+    :return: volume for profile
+    """
+
     results = []
 
     for z in dz:
