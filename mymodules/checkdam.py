c8529592ea77eaaf5a1588f9e28ead1a8dd0a19a
Kirubaharan <haran.kiruba@gmail.com>
2014-07-09 16:39:20 +0530

    added function as module

---
+++ mymodules/checkdam.py
@@ -0,0 +1,34 @@
+__author__ = 'kiruba'
+import itertools
+
+def pairwise(iterable):
+    """s -> (s0,s1), (s1,s2), (s2,s3), ..."""
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
+
+#function to create stage volume output
+
+def calcvolume(profile, order, dy):
+    """
+    Profile = df.Y1,df.Y2,.. and order = 1,2,3
+    :param profile: series of Y values
+    :param order: distance from origin
+    :param dy: thickness of profile in m
+    :return: volume for profile
+    """
+
+    results = []
+
+    for z in dz:
+        water_area = 0
+        for y1, y2 in pairwise(profile):
+            delev = (y2 - y1) / 10
+            elev = y1
+            for b in range(1, 11, 1):
+                elev += delev
+                if z > elev:
+                    water_area += (0.1 * (z-elev))
+        calc_vol = water_area * dy
+        results.append(calc_vol)
+    output[('Volume_%s' % order)] = results
--- stage_area.py
+++ stage_area.py
@@ -8,4 +8,41 @@ from spread import sp
 # copy the code from http://code.activestate.com/recipes/577878-generate-equally-spaced-floats/ #
 import itertools
 from matplotlib import rc
+from linecache import getline
+
+
+def floodfill(c, r, mask):
+    """
+    Crawls a mask array containing
+    only 1 and 0 values from the starting
+    point (c=column, r=row-a.k.a.x,y) and returns
+    an array with all 1 values connected to the starting
+     cell. This algorithm performs a 4-way check non-recursively
+    :param c: column
+    :param r: row -a.k.a
+    :param mask:
+    :return:
+    """
+    # cells already filled
+    filled = set()
+    # cells to fill
+    fill = set()
+    fill.add((c,r))
+    width = mask.shape[1]-1
+    height = mask.shape[0]-1
+    # Our output inundation array
+    flood = np.zeros_like(mask,dtype=np.int8)
+    # Loop through and modify the cells which need to be checked
+    while fill:
+        #grab a cell
+        x,y = fill.pop()
+        if y == height or x ==width or x < 0 or y < 0:
+            #Dont fill
+            continue
+
+
+
+
+
+
 
