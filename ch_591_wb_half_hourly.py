39df42caace289be80d59eb1f1bfa754fb965edf
Kirubaharan <haran.kiruba@gmail.com>
2014-11-04 11:32:47 +0530

    half_hour Evaporation function

--- ch_591_wb_half_hourly.py
+++ ch_591_wb_half_hourly.py
@@ -11,6 +11,9 @@ import email.utils as eutils
 import time
 import datetime
 from datetime import timedelta
+import scipy as sp
+import meteolib as met
+import evaplib
 
 # latex parameters
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
@@ -73,51 +76,6 @@ a_stage = polyfit(x_cal, y_cal, 1)
 # coefficients of polynomial are stored in following list
 coeff_cal = a_stage['polynomial']
 
-# water_level_1 = pd.read_csv(block_1, skiprows=9, sep=',', header=0,  names=['scan no', 'date',
-#                                                                             'time', 'raw value', 'calibrated value'])
-# water_level_1['calibrated value'] = (water_level_1['raw value']*coeff_cal[0]) + coeff_cal[1]  # in cm
-# # convert to metre
-# water_level_1['calibrated value'] /= 100
-# #change the column name
-# water_level_1.columns.values[4] = 'stage(m)'
-# # create date time index
-# format = '%d/%m/%Y  %H:%M:%S'
-# # change 24:00:00 15 May to 00:00:00 16 May
-#
-# # water_level_1['time'] = water_level_1['time'].replace(' 24:00:00', ' 23:59:59')
-#
-# # water_level_1['date'] = pd.to_datetime(water_level_1['date'], format='%d/%m/%Y ')
-# # print water_level_1.head(20)
-# # c_str = water_level_1['time'][11]
-# c_str = ' 24:00:00'
-# # print(c_str)
-# for index, row in water_level_1.iterrows():
-#     x_str = row['time']
-#     # store stings in object and then compare
-#     if x_str == c_str:
-#         # print row['date']
-#         # x_date = row['date']
-#         # print x_date
-#         # convert string to datetime object
-#         r_date = pd.to_datetime(row['date'], format='%d/%m/%Y ')
-#         # print r_date
-#         # add 1 day
-#         c_date = r_date + timedelta(days=1)
-#         # convert datetime to string
-#         c_date = c_date.strftime('%d/%m/%Y ')
-#         # print(c_date)
-#         # print row['date']
-#         c_time = ' 00:00:00'
-#         water_level_1['date'][index] = c_date
-#         water_level_1['time'][index] = c_time
-#
-# water_level_1['date_time'] = pd.to_datetime(water_level_1['date'] + water_level_1['time'], format=format)
-# water_level_1.set_index(water_level_1['date_time'], inplace=True)
-# # # drop unneccessary columns before datetime aggregation
-# water_level_1.drop(['scan no', 'date', 'time', 'raw value', 'date_time'], inplace=True, axis=1)
-#
-# print water_level_1.head(20)
-
 
 def read_correct_ch_dam_data(csv_file):
     """
@@ -175,3 +133,217 @@ weather_df = weather_df.join(rain_df, how='right')
 weather_df = weather_df[min(water_level.index): max(water_level.index)]
 weather_df = weather_df.join(water_level, how='right')
 # print weather_df.head(20)
+
+"""
+Evaporation from open water
+Equation according to J.D. Valiantzas (2006). Simplified versions
+for the Penman evaporation equation using routine weather data.
+J. Hydrology 331: 690-702. Following Penman (1948,1956). Albedo set
+at 0.06 for open water.
+Input (measured at 2 m height):
+        - airtemp: (array of) average air temperatures [Celsius]
+        - rh: (array of)  average relative humidity [%]
+        - airpress: (array of) average air pressure data [Pa]
+        - Rs: (array of) incoming solar radiation [J/m2/day]
+        - N: (array of) maximum daily sunshine hours [h]
+        - Rext: (array of) daily extraterrestrial radiation [J/m2/day]
+        - u: (array of) daily average wind speed at 2 m [m/s]
+        - Z: (array of) site elevation [m a.s.l.], default is zero...
+
+    Output:
+        - E0: (array of) Penman open water evaporation values [mm/day]
+
+"""
+"""
+ air pressure (Pa) = 101325(1-2.25577 10^-5 h)^5.25588
+h = altitude above sea level (m)
+http://www.engineeringtoolbox.com/air-altitude-pressure-d_462.html
+mean elevation over watershed = 803.441589 m
+Elevation at the check dam = 799 m
+"""
+z = 799
+p = (1-(2.25577*(10**-5)*z))
+air_p_pa = 101325*(p**5.25588)
+  # give air pressure value
+weather_df['AirPr(Pa)'] = air_p_pa
+"""
+Half hourly Extraterrestrial Radiation Calculation(J/m2/30min)
+"""
+# yet to work on
+
+def rext_calc(df, lat=float):
+    """
+    Function to calculate extraterrestrial radiation output in MJ/m2/30 min
+    Ref:http://www.fao.org/docrep/x0490e/x0490e07.htm
+
+    :param df: dataframe with datetime index
+    :param lat: latitude (negative for Southern hemisphere)
+    :return: Rext (MJ/m2/30min)
+    """
+    # set solar constant [MJ m^-2 min^-1]
+    s = 0.08166
+    #convert latitude [degrees] to radians
+    latrad = lat*math.pi / 180.0
+    #have to add in function for calculating single value here
+    # extract date, month, year from index
+    date = pd.DatetimeIndex(df.index).day
+    month = pd.DatetimeIndex(df.index).month
+    year = pd.DatetimeIndex(df.index).year
+    doy = met.date2doy(dd=date, mm=month, yyyy=year)  # create day of year(1-366) acc to date
+    l = sp.size(doy)
+    if l < 2:
+        dt = 0.409 * math.sin(2 * math.pi / 365 * doy - 1.39)
+        ws = sp.arccos(-math.tan(latrad) * math.tan(dt))
+        j = 2 * math.pi / 365.25 * doy
+        dr = 1.0 + 0.03344 * math.cos(j - 0.048869)
+        rext = s * 1440 / math.pi * dr * (ws * math.sin(latrad) * math.sin(dt) + math.sin(ws) * math.cos(latrad) * math.cos(dt))
+    #Create dummy output arrays sp refers to scipy
+    else:
+        rext = sp.zeros(l)
+        dt = sp.zeros(l)
+        ws = sp.zeros(l)
+        j = sp.zeros(l)
+        dr = sp.zeros(l)
+        #calculate Rext
+        for i in range(0, l):
+            #Calculate solar decimation dt(d in FAO) [rad]
+            dt[i] = 0.409 * math.sin(2 * math.pi / 365 * doy[i] - 1.39)
+            #calculate sunset hour angle [rad]
+            ws[i] = sp.arccos(-math.tan(latrad) * math.tan(dt[i]))
+            # calculate day angle j [radians]
+            j[i] = 2 * math.pi / 365.25 * doy[i]
+            # calculate relative distance to sun
+            dr[i] = 1.0 + 0.03344 * math.cos(j[i] - 0.048869)
+            #calculate Rext dt = d(FAO) and latrad = j(FAO)
+            rext[i] = (s * 30.0 / math.pi) * dr[i] * (ws[i] * math.sin(latrad) * math.sin(dt[i]) + math.sin(ws[i])* math.cos(latrad) * math.cos(dt[i]))
+
+    rext = sp.array(rext)
+    return rext
+
+weather_df['Rext (MJ/m2/30min)'] = rext_calc(weather_df, lat=13.260196)
+"""
+wind speed from km/h to m/s
+1 kmph = 0.277778 m/s
+"""
+weather_df['Wind Speed (mps)'] = weather_df['Wind Speed (kmph)'] * 0.277778
+"""
+Radiation unit conversion
+"""
+# the radiation units are actually in W/m2 and
+#  not in W/mm2 as given by weather station,
+# so multiply with 30*60 seconds
+# to convert to MJ divide by 10^6
+weather_df['Solar Radiation (MJ/m2/30min)'] = (weather_df['Solar Radiation (W/mm2)'] * 1800)/(10**6)
+"""
+Average Temperature Calculation
+"""
+weather_df['Average Temp (C)'] = 0.5*(weather_df['Min Air Temperature (C)'] + weather_df['Max Air Temperature (C)'])
+
+"""
+Open water evaporation function for half hour
+Modified from evaplib.py
+http://python.hydrology-amsterdam.nl/moduledoc/index.html#module-evaplib
+"""
+
+
+def half_hour_E0(airtemp = sp.array([]),
+                 rh = sp.array([]),
+                 airpress = sp.array([]),
+                 Rs = sp.array([]),
+                 Rext = sp.array([]),
+                 u =sp.array([]),
+                 Z=0.0):
+    """
+     Function to calculate daily Penman open water evaporation (in mm/30min).
+    Equation according to J.D. Valiantzas (2006). Simplified versions
+    for the Penman evaporation equation using routine weather data.
+    J. Hydrology 331: 690-702. Following Penman (1948,1956). Albedo set
+    at 0.06 for open water.
+
+    :param airtemp: average air temperature [Celsius]
+    :param rh: relative humidity[%]
+    :param airpress: average air pressure[Pa]
+    :param Rs: Incoming solar radiation [MJ/m2/30min]
+    :param Rext: Extraterrestrial radiation [MJ/m2/30min]
+    :param u: average wind speed at 2 m from ground [m/s]
+    :param Z: site elevation, default is zero [metre]
+    :return: Penman open water evaporation values [mm/30min]
+    """
+     # Set constants
+    albedo = 0.06  # open water albedo
+    # Stefan boltzmann constant = 5.670373*10-8 J/m2/k4/s
+    # http://en.wikipedia.org/wiki/Stefan-Boltzmann_constant
+    # sigma = 5.670373*(10**-8)  # J/m2/K4/s
+    sigma = (1.02066714*(10**-10))  #Stefan Boltzmann constant MJ/m2/K4/30min
+    # Calculate Delta, gamma and lambda
+    DELTA = met.Delta_calc(airtemp)     # [Pa/K]
+    gamma = met.gamma_calc(airtemp, rh, airpress)   # [Pa/K]
+    Lambda = (met.L_calc(airtemp))/(10**6)      # [MJ/kg]
+    # Calculate saturated and actual water vapour pressure
+    es = met.es_calc(airtemp)  # [Pa]
+    ea = met.ea_calc(airtemp,rh)  # [Pa]
+    #Determine length of array
+    l = sp.size(airtemp)
+    #Check if we have a single value or an array
+    if l < 2:
+        Rns = (1.0 - albedo)* Rs  # shortwave component [MJ/m2/30min]
+        #calculate clear sky radiation Rs0
+        Rs0 = (0.75+(2E-5*Z))*Rext
+        f = (1.35*(Rs/Rs0))-0.35
+        epsilom = 0.34-(-0.14*sp.sqrt(ea/1000))
+        Rnl = f*epsilom*sigma*(airtemp+273.16)**4  # Longwave component [MJ/m2/30min]
+        Rnet = Rns - Rnl
+        Ea = (1 + (0.536*u))*((es/1000)-(ea/1000))
+        E0 = ((DELTA*Rnet) + gamma*(6.43*Ea))/(Lambda*(DELTA+gamma))
+    else:
+        # Inititate output array
+        E0 = sp.zeros(l)
+        Rns = sp.zeros(l)
+        Rs0 = sp.zeros(l)
+        f = sp.zeros(l)
+        epsilom = sp.zeros(l)
+        Rnl = sp.zeros(l)
+        Rnet = sp.zeros(l)
+        Ea = sp.zeros(l)
+        for i in range(0,l):
+            # calculate longwave radiation (MJ/m2/30min)
+            Rns[i] = (1.0 - albedo) * Rs[i]
+            # calculate clear sky radiation Rs0
+            Rs0[i] = (0.75 + (2E-5*Z))
+            f[i] = (1.35*(Rs[i]/Rs0[i]))-0.35
+            epsilom[i] = 0.34-(-0.14*sp.sqrt(ea[i]/1000))
+            Rnl[i] = f[i]*epsilom[i]*sigma*(airtemp[i]+273.16)**4  # Longwave component [MJ/m2/30min]
+            Rnet[i] = Rns[i] - Rnl[i]
+            Ea[i] = (1 + (0.536*u[i]))*((es[i]/1000)-(ea[i]/1000))
+            E0[i] = ((DELTA[i]*Rnet[i]) + gamma[i]*(6.43*Ea[i]))/(Lambda[i]*(DELTA[i]+gamma[i]))
+    return E0
+
+
+"""
+Half hourly Evaporation calculation
+"""
+airtemp = weather_df['Average Temp (C)']
+hum = weather_df['Humidity (%)']
+airpress = weather_df['AirPr(Pa)']
+rs = weather_df['Solar Radiation (MJ/m2/30min)']
+rext = weather_df['Rext (MJ/m2/30min)']
+wind_speed = weather_df['Wind Speed (mps)']
+weather_df['Evaporation (mm/30min)'] = half_hour_E0(airtemp=airtemp, rh=hum, airpress=airpress,
+                                                    Rs=rs, Rext=rext, u=wind_speed, Z=z)
+"""
+Plot Evaporation
+"""
+fig = plt.figure(figsize=(11.69, 8.27))
+plt.plot_date(weather_df.index, weather_df['Evaporation (mm/30min)'], '-g', label='Evaporation (mm/30min)')
+plt.ylabel(r'\textbf{Evaporation ($mm/30min$)}')
+fig.autofmt_xdate(rotation=90)
+plt.title(r"Daily Evaporation for Check Dam - 591", fontsize=20)
+plt.savefig('/media/kiruba/New Volume/ACCUWA_Data/python_plots/check_dam_591/evaporation_591_30min')
+# bar plots
+weather_sel_df = weather_df['2014-05-20':'2014-05-22']
+fig = plt.figure(figsize=(11.69, 8.27))
+plt.plot_date(weather_sel_df.index, weather_sel_df['Evaporation (mm/30min)'], '-g')
+fig.autofmt_xdate(rotation=90)
+plt.savefig('/media/kiruba/New Volume/ACCUWA_Data/python_plots/check_dam_591/evaporation_591_may20_22')
+
+plt.show()
