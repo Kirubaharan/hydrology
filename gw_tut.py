c07495cf7231d968d6d406cb0974c62df4089086
Kirubaharan <haran.kiruba@gmail.com>
2015-02-27 17:05:30 +0530

    gw_tut

--- gw_tut.py
+++ gw_tut.py
@@ -7,6 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import fortranfile as ff
 import flopy
+from array import array
+from struct import *
+from scipy.interpolate import griddata
 print flopy.__version__
 
 # from numpy import array
@@ -91,17 +94,29 @@ oc = fmf.ModflowOc(ml)
 ml.write_input()
 ml.run_model()
 
-hds = fut.HeadFile(ml.model_ws+name+'.hds')
-h = hds.get_data(kstpker=(1,1))
-x = y = np.linspace(0, L, N)
-a = ff.FortranFile("lake_example.hds",mode='r')
-head = a.readReals()
-xx = a.readInts()
-c = plt.contour(x, y, head , np.arange(90, 100.1, 0.2))
-plt.show()
-print len(x)
-print len(xx)
+# hds = fut.HeadFile(ml.model_ws+name+'.hds')
+# h = hds.get_data(kstpker=(1,1))
+# x = y = np.linspace(0, L, N)
+# a = ff.FortranFile("lake_example.hds",endian='<', header_prec='i')
+# x = a.readInts()
+# xx = a.readReals('')
+# print x
+# print xx.shape
+# print xx
+# print len(x)
+# print len(xx)
+# x = y = np.linspace(0, L, N)
+# print len(x)
+# xi = yi = np.linspace(0, L, 500)
+# zi = griddata((x,y), xx, (xi[None, :], yi[:, None]), method='linear')
+# fig = plt.figure()
+# c = plt.contour(xi, yi, zi, 15)
+# plt.show()
 # # a = ff.FortranFile("lake_example.hds",mode='w')
 # # a.writeReals(np.linspace(0,1,10))
 # # a.close()
 # # hds = fut.HeadFile('lake_example.hds')
+floattype = 'f4'
+a = np.fromfile("lake_example.hds", np.dtype([('kstp','i4'),('kper','i4'),('pertim',floattype),('totim',floattype),('text','a16'),('ncol','i4'),('nrow','i4'),('ilay','i4')]))
+print a
+print a.shape
